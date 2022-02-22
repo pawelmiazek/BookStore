a9bcacda549b06aacb97b5fb5e022f7651603f73
Paweł Miążek <p.miazek@merixstudio.com>
2022-02-22 22:28:08 +0100

    Add API tests

--- tasks/factories.py
+++ tasks/factories.py
@@ -1,1 +1,35 @@
-# import factory
+import factory
+from django.contrib.auth import get_user_model
+from faker import Factory
+
+from tasks.models import Account, Book
+
+User = get_user_model()
+faker = Factory.create()
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    first_name = factory.LazyAttribute(lambda _: faker.word())
+    last_name = factory.LazyAttribute(lambda _: faker.word())
+    username = factory.LazyAttribute(lambda _: faker.word())
+    email = factory.LazyAttribute(
+        lambda u: "{}.{}@example.com".format(u.first_name, u.last_name).lower()
+    )
+
+
+class AccountFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Account
+
+    user = factory.SubFactory(UserFactory)
+
+
+class BookFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Book
+
+    title = factory.LazyAttribute(lambda _: faker.word())
+    price = factory.Sequence(lambda n: "%d" % (n + 50))
--- tasks/tests.py
+++ tasks/tests.py
@@ -1,1 +1,52 @@
-# import pytest
+from decimal import Decimal
+
+import pytest
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from tasks.factories import AccountFactory, BookFactory
+
+
+@pytest.fixture
+def factory_account(db):
+    account = AccountFactory(current_balance=Decimal(2000))
+    return account
+
+
+@pytest.fixture
+def factory_books(db):
+    books = BookFactory.create_batch(4)
+    return books
+
+
+@pytest.mark.django_db
+def test_book_list_allow_any():
+    client = APIClient()
+    url = "/books/"
+
+    response = client.get(url)
+
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_book_buy_allow_any(factory_books):
+    client = APIClient()
+    url = "/books/buy/"
+    data = {"books": [book.id for book in factory_books]}
+
+    response = client.post(url, data=data)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_book_buy_is_authenticated(factory_account, factory_books):
+    client = APIClient()
+    client.force_authenticate(user=factory_account.user)
+    url = "/books/buy/"
+    data = {"books": [book.id for book in factory_books]}
+
+    response = client.post(url, data=data)
+
+    assert response.status_code == status.HTTP_201_CREATED
